dc4ec684f8fb02dd9ae0d4b94bdb47653b47fa17
zethriller <thriller.ze@gmail.com>
2022-04-19 21:10:52 +0200

    Rework of DV2TotalsPane sub-control using skia

--- MidsReborn/Forms/Controls/DV2TotalsPane.Designer.cs
+++ MidsReborn/Forms/Controls/DV2TotalsPane.Designer.cs
@@ -28,47 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.gradientPanel1 = new Syncfusion.Windows.Forms.Tools.GradientPanel();
-            this.panel1 = new System.Windows.Forms.Panel();
-            ((System.ComponentModel.ISupportInitialize)(this.gradientPanel1)).BeginInit();
+            this.skglControl1 = new SkiaSharp.Views.Desktop.SKGLControl();
             this.SuspendLayout();
             // 
-            // gradientPanel1
+            // skglControl1
             // 
-            this.gradientPanel1.BackgroundColor = new Syncfusion.Drawing.BrushInfo(Syncfusion.Drawing.GradientStyle.Horizontal, System.Drawing.Color.Black, System.Drawing.Color.Purple);
-            this.gradientPanel1.Border3DStyle = System.Windows.Forms.Border3DStyle.Flat;
-            this.gradientPanel1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.gradientPanel1.Location = new System.Drawing.Point(140, 0);
-            this.gradientPanel1.Name = "gradientPanel1";
-            this.gradientPanel1.Size = new System.Drawing.Size(45, 138);
-            this.gradientPanel1.TabIndex = 0;
-            // 
-            // panel1
-            // 
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(134, 138);
-            this.panel1.TabIndex = 1;
+            this.skglControl1.BackColor = System.Drawing.Color.Black;
+            this.skglControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.skglControl1.Location = new System.Drawing.Point(0, 0);
+            this.skglControl1.Name = "skglControl1";
+            this.skglControl1.Size = new System.Drawing.Size(185, 138);
+            this.skglControl1.TabIndex = 0;
+            this.skglControl1.VSync = false;
             // 
             // DV2TotalsPane
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.gradientPanel1);
+            this.Controls.Add(this.skglControl1);
             this.Margin = new System.Windows.Forms.Padding(0);
             this.Name = "DV2TotalsPane";
             this.Size = new System.Drawing.Size(185, 138);
-            ((System.ComponentModel.ISupportInitialize)(this.gradientPanel1)).EndInit();
             this.ResumeLayout(false);
-
         }
 
         #endregion
 
-        private Syncfusion.Windows.Forms.Tools.GradientPanel gradientPanel1;
-        private System.Windows.Forms.Panel panel1;
+        private SkiaSharp.Views.Desktop.SKGLControl skglControl1;
     }
 }
--- MidsReborn/Forms/Controls/DV2TotalsPane.cs
+++ MidsReborn/Forms/Controls/DV2TotalsPane.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.Windows.Forms;
-using mrbControls;
-using Syncfusion.Drawing;
+using SkiaSharp;
+using SkiaSharp.Views.Desktop;
 
 namespace Mids_Reborn.Forms.Controls
 {
-    public partial class DV2TotalsPane : UserControl
+    public partial class DV2TotalsPane : UserControl, IDrawLock
     {
         private List<Item> Items = new();
-        private Color _BackgroundColorEnd;
         private int VisibleItemsCount => Math.Min(Items.Count, MaxItems);
         private const int BarHeight = 10;
         private const float LabelsFontSize = 7;
+        private bool _DrawLock;
 
         [Description("Maximum visible items"), Category("Data"),
          Browsable(true), EditorBrowsable(EditorBrowsableState.Always), Bindable(true),
@@ -25,35 +25,22 @@ namespace Mids_Reborn.Forms.Controls
          Browsable(true), EditorBrowsable(EditorBrowsableState.Always), Bindable(true),
          DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
         public float GlobalMaxValue;
-        
-        [Description("Bar stat group"), Category("Data"),
-         Browsable(true), EditorBrowsable(EditorBrowsableState.Always), Bindable(true),
-         DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
-        public string Group;
 
         [Description("Bars background gradient end color"), Category("Appearance"),
          Browsable(true), EditorBrowsable(EditorBrowsableState.Always), Bindable(true),
          DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
-        public Color BackgroundColorEnd
-        {
-            get => _BackgroundColorEnd;
-            set
-            {
-                _BackgroundColorEnd = value;
-                gradientPanel1.BackgroundColor = new BrushInfo(GradientStyle.Horizontal,Color.Black, _BackgroundColorEnd);
-            }
-        }
+        public Color BackgroundColorEnd;
 
         [Description("Bars color (main value)"), Category("Appearance"),
          Browsable(true), EditorBrowsable(EditorBrowsableState.Always), Bindable(true),
          DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
         public Color BarColorMain;
-        
+
         [Description("Bars color (uncapped value)"), Category("Appearance"),
          Browsable(true), EditorBrowsable(EditorBrowsableState.Always), Bindable(true),
          DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
         public Color BarColorUncapped;
-        
+
         [Description("Enable uncapped values (dual bars)"), Category("Data"),
          Browsable(true), EditorBrowsable(EditorBrowsableState.Always), Bindable(true),
          DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
@@ -62,122 +49,169 @@ namespace Mids_Reborn.Forms.Controls
         public DV2TotalsPane()
         {
             SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint, true);
+
+            Load += DV2TotalsPane_Load;
+            Resize += DV2TotalsPane_Resize;
+
             InitializeComponent();
+            skglControl1.PaintSurface += skglControl1_PaintSurface;
         }
 
-        public void ClearItems()
+        public void LockDraw()
         {
-            Items = new List<Item>();
+            _DrawLock = true;
         }
 
-        public void AddItem(Item item)
+        public void UnlockDraw(bool redraw = true)
+        {
+            _DrawLock = false;
+            if (!redraw)
+            {
+                return;
+            }
+
+            Draw();
+        }
+
+        public void ClearItems(bool redraw = false)
+        {
+            Items.Clear();
+            if (!redraw)
+            {
+                return;
+            }
+
+            Draw();
+        }
+
+        public void AddItem(Item item, bool redraw = false)
         {
             Items.Add(item);
-        }
-
-        private void DrawBars()
-        {
-            gradientPanel1.Controls.Clear();
-            for (var i = 0; i < Math.Min(Items.Count, VisibleItemsCount); i++)
+            if (!redraw)
             {
-                var bar = new ctlLayeredBarPb
-                {
-                    MinimumBarValue = 0,
-                    MaximumBarValue = GlobalMaxValue,
-                    Size = new Size(gradientPanel1.Width - 2, BarHeight),
-                    Location = new Point(1, (1 + i) * BarHeight),
-                    Group = Group
-                };
-
-                if (EnableUncappedValues)
-                {
-                    bar.AssignNames(new List<(string name, Color color)>
-                    {
-                        (name: "value", color: BarColorMain),
-                        (name: "uncapped", color: BarColorUncapped)
-                    });
-
-                    bar.AssignValues(new List<(string name, float value)>
-                    {
-                        (name: "value", value: Items[i].Value),
-                        (name: "uncapped", value: Items[i].UncappedValue)
-                    });
-                }
-                else
-                {
-                    bar.AssignNames(new List<(string name, Color color)>
-                    {
-                        (name: "value", color: BarColorMain)
-                    });
-
-                    bar.AssignValues(new List<(string name, float value)>
-                    {
-                        (name: "value", value: Items[i].Value)
-                    });
-                }
-
-                gradientPanel1.Controls.Add(bar);
-            }
-        }
-
-        private void DrawLabels()
-        {
-            var grid = new TableLayoutPanel
-            {
-                ColumnCount = 2,
-                RowCount = Math.Min(Items.Count, MaxItems),
-                BackColor = Color.Black,
-                Dock = DockStyle.Fill
-            };
-
-            grid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 60));
-            grid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 40));
-
-            for (var i = 0; i < Math.Min(Items.Count, VisibleItemsCount); i++)
-            {
-                grid.RowStyles.Add(new RowStyle(SizeType.Percent, 12));
-                var labelTag = new Label
-                {
-                    ForeColor = Color.White,
-                    BackColor = Color.Black,
-                    TextAlign = ContentAlignment.MiddleLeft,
-                    Dock = DockStyle.Fill,
-                    AutoSize = false,
-                    Text = $"{Items[i].Name}:",
-                    Font = new Font(FontFamily.GenericSansSerif, LabelsFontSize, FontStyle.Regular, GraphicsUnit.Pixel)
-                };
-
-                var labelValue = new Label
-                {
-                    ForeColor = Color.White,
-                    BackColor = Color.Black,
-                    TextAlign = ContentAlignment.MiddleRight,
-                    Dock = DockStyle.Fill,
-                    AutoSize = false,
-                    Text = $"{Items[i].Value:####.##}{(Items[i].DisplayPercentage ? "%" : "")}",
-                    Font = new Font(FontFamily.GenericSansSerif, LabelsFontSize, FontStyle.Regular, GraphicsUnit.Pixel)
-                };
-
-                grid.Controls.Add(labelTag, 0, i);
-                grid.Controls.Add(labelValue, 1, i);
+                return;
             }
 
-            panel1.Controls.Clear();
-            panel1.Controls.Add(grid);
+            Draw();
+        }
+
+        private static SKColor FromColor(Color c)
+        {
+            return new SKColor(c.R, c.G, c.B);
         }
 
         public void Draw()
         {
-            DrawLabels();
-            DrawBars();
+            if (_DrawLock)
+            {
+                return;
+            }
+
+            skglControl1.Invalidate();
+        }
+
+        private void DrawOutlineText(SKCanvas canvas, string text, SKPoint location, SKColor textColor, float fontSize = 12f, float strokeWidth = 5f)
+        {
+            using var textFont = new SKFont(SKTypeface.Default, fontSize);
+            using var textPaint = new SKPaint(textFont)
+            {
+                IsAntialias = true,
+                IsStroke = false,
+                Color = textColor
+            };
+
+            var textBounds = new SKRect();
+            textPaint.MeasureText(text, ref textBounds);
+
+            using var textPath = textPaint.GetTextPath(text, location.X, location.Y); // ??? - centered text not handled
+            using var outlinePath = new SKPath();
+            textPaint.GetFillPath(textPath, outlinePath);
+
+            using var outlinePaint = new SKPaint
+            {
+                StrokeCap = SKStrokeCap.Round,
+                StrokeMiter = 0, /* Avoid spikes artifacts around sharp edges */
+                Style = SKPaintStyle.Stroke,
+                StrokeWidth = strokeWidth,
+                Color = SKColors.Black
+            };
+
+            canvas.DrawPath(outlinePath, outlinePaint);
+            canvas.DrawText(text, location, textPaint);
+        }
+
+        private void DV2TotalsPane_Load(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void DV2TotalsPane_Resize(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void skglControl1_PaintSurface(object sender, SKPaintGLSurfaceEventArgs e)
+        {
+            const float barWidthFactor = 0.333f;
+
+            e.Surface.Canvas.Clear(SKColors.Black);
+
+            using var bgGradientPaint = new SKPaint
+            {
+                Shader = SKShader.CreateLinearGradient(
+                    new SKPoint(0, 0), new SKPoint(Width, 0),
+                    new[] { SKColors.Black, SKColors.Black, FromColor(BackgroundColorEnd) },
+                    new[] { 0, 1 - barWidthFactor, 1 },
+                    SKShaderTileMode.Clamp
+                )
+            };
+
+            e.Surface.Canvas.DrawRect(new SKRect(0, 0, Width, Height), bgGradientPaint);
+
+            using var barBg = new SKPaint { Color = SKColors.Black };
+            using var barUncapped = new SKPaint { Color = FromColor(BarColorUncapped) };
+            using var textPaint = new SKPaint { Color = SKColors.WhiteSmoke };
+            using var outlinePaint = new SKPaint { Color = SKColors.Black };
+
+            var xStart = (int) Math.Round(Width / (1 - barWidthFactor)) - 2;
+            for (var i = 0; i < Math.Min(Items.Count, VisibleItemsCount); i++)
+            {
+                var barGradientPaint = new SKPaint
+                {
+                    Shader = SKShader.CreateLinearGradient(
+                        new SKPoint(xStart + 1, 0), new SKPoint(Width, 0),
+                        new[] { new SKColor((byte) Math.Round(BarColorMain.R / 4f), (byte) Math.Round(BarColorMain.G / 4f), (byte) Math.Round(BarColorMain.R / 4f)), FromColor(BarColorMain) }, // ???
+                        new[] { 0, 1f },
+                        SKShaderTileMode.Clamp
+                    )
+                };
+
+                if (EnableUncappedValues)
+                {
+                    var scale = Math.Max(Items[i].Value, Items[i].UncappedValue) / GlobalMaxValue * ((Width - 1) * barWidthFactor);
+                    e.Surface.Canvas.DrawRect(new SKRect(xStart, i * 12 + 2, Width - 2, i * 12 + 12), barBg);
+                    e.Surface.Canvas.DrawRect(new SKRect(xStart + 1, i * 12 + 3, xStart + 1 + Items[i].UncappedValue * scale, i * 12 + 12), barUncapped);
+                    e.Surface.Canvas.DrawRect(new SKRect(xStart + 1, i * 12 + 3, xStart + 1 + Items[i].Value * scale, i * 12 + 12), barGradientPaint);
+                }
+                else
+                {
+                    var scale = Items[i].Value / GlobalMaxValue * ((Width - 1) * barWidthFactor);
+                    e.Surface.Canvas.DrawRect(new SKRect(xStart, i * 12 + 2, Width - 2, i * 12 + 12), barBg);
+                    //e.Surface.Canvas.DrawRect(new SKRect(xStart + 1, i * 12 + 3, xStart + 1 + Items[i].UncappedValue * scale, i * 12 + 12), barUncapped);
+                    e.Surface.Canvas.DrawRect(new SKRect(xStart + 1, i * 12 + 3, xStart + 1 + Items[i].Value * scale, i * 12 + 12), barGradientPaint);
+                }
+
+                //e.Surface.Canvas.DrawText(Items[i].Name, new SKPoint(2, i * 12 + 4), textPaint);
+                DrawOutlineText(e.Surface.Canvas, Items[i].Name, new SKPoint(2, i * 12 + 4), SKColors.WhiteSmoke);
+            }
         }
 
         #region Table label sub-class
 
         public class Item
         {
-            private float _Value;
-            private float _UncappedValue;
+            private readonly float _Value;
+            private readonly float _UncappedValue;
 
             public string Name { get; set; }
             public float Value => DisplayPercentage ? _Value * 100 : _Value;
@@ -194,12 +228,5 @@ namespace Mids_Reborn.Forms.Controls
         }
 
         #endregion
-
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            base.OnPaint(e);
-
-            Draw();
-        }
     }
 }
